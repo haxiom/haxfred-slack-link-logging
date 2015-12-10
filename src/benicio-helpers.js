4fcbdd11a923d8ec8b7825352b10ffca5718b792
Blade Barringer <blade@crookedneighbor.com>
2015-12-09 17:16:55 -0800

    feat: Add helper module to send link data to benicio server

---
+++ src/benicio-helpers.js
@@ -0,0 +1,9 @@
+import request from 'superagent';
+
+export function post (endpoint, details, cb) {
+  request.post(endpoint)
+    .type('form')
+    .accept('application/json')
+    .send(details)
+    .end(cb);
+}
