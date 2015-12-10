771da2cc846114e33556a4da3419728060605181
Blade Barringer <blade@crookedneighbor.com>
2015-12-09 17:15:43 -0800

    chore(setup): Add setup for mocha config

---
+++ test/helpers/globals.helper.js
@@ -0,0 +1,12 @@
+/* eslint-disable no-undef, no-shadow */
+//------------------------------
+// Global modules
+//------------------------------
+
+import chai from 'chai';
+import sinon from 'sinon';
+
+chai.use(require('sinon-chai'));
+
+global.sinon = sinon;
+global.expect = chai.expect;
